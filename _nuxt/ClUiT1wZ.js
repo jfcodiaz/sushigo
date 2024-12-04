33e6e13f6e5e61fcaeb1d71b2f0c42b386603239
JFcoDiaz <jfcodiaz@gmail.com>
2024-12-04 02:51:25 -0600

    Updates

--- _nuxt/ClUiT1wZ.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./rwO1qAmf.js";import{u as e,m as n,n as u,o as r}from"./BVjv0jFl.js";import"./CoOMy024.js";const g={__name:"[slug]",setup(_){const o=e().params.slug;return(c,m)=>{const s=t;return r(),n(s,{slug:u(o)},null,8,["slug"])}}};export{g as default};
